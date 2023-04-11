adbaa26ca0cdd9a2328d1da44b42e25e3873d71e
TimBMK <tim.b.m.koenig@web.de>
2023-04-11 11:56:38 +0200

    updated seed account generation; updated structure

--- Seed_Accounts/generate_account_list.R
+++ Seed_Accounts/generate_account_list.R
@@ -14,6 +14,47 @@ account_list <- read_csv("EPINetz_seedlist_2023-01-02.csv",
                                           user_id = "c",
                                           abgeordnetenwatch_id = "c"))
 
+
+#### Ministries ####
+
+ministries <- read_csv("ministries_accounts.csv", 
+                       col_types = list(id = "c")) %>% 
+  select(!`...1`)
+
+
+## add policy field
+
+ministries <- ministries %>% mutate(policy_field = case_when(
+  username == "BMAS_Bund" ~ "arbeit",
+  username == "AuswaertigesAmt" ~ "aeusseres",
+  username == "BMZ_Bund" ~ "entwicklung",
+  username == "BMBF_Bund" ~ "bildung_forschung",
+  username == "bmj_bund" ~ "gesellschaft",
+  username == "BMG_Bund" ~ "gesundheit",
+  username == "BMF_Bund" ~ "finanzen_haushalt",
+  username == "BMI_Bund" ~ "innere_sicherheit",
+  username == "bmel" ~ "landwirtschaft_ernaehrung",
+  username == "BMFSFJ" ~ "soziales",
+  username == "BMUV" ~ "umwelt",
+  username == "bmdv" ~ "verkehr",
+  username == "BMVg_Bundeswehr" ~ "verteidigung",
+  username == "BMWK" ~ "wirtschaft",
+  username == "BMWSB_Bund" ~ "soziales"
+))
+
+
+## clean and save
+
+ministries <- ministries %>% rename(official_name = name, user_id = id) %>% 
+  select(!username)
+
+ministries %>% mutate(across(.cols = where(is.character),  ~ utf8::as_utf8(.x))) %>% 
+  write_csv("ministry_seeds_2023-04-06.csv")
+                        
+
+
+##### Committees #####
+
 # WP 19
 
 ## Read list of committee members 
@@ -50,6 +91,31 @@ committee_members_19 <- committee_members_19 %>% mutate(period = "WP_19",
                                                         begin = ymd("2017-10-24"),
                                                         end = ymd("2021-10-26"))
 
+## add policy field
+
+committee_members_19 <- committee_members_19 %>% mutate(policy_field = case_when(
+  committee == "Arbeit und Soziales" ~ "arbeit",
+  committee == "Auswärtiger Ausschuss" ~ "aeusseres",
+  committee == "Menschenrechte und humanitäre Hilfe" ~ "entwicklung",
+  committee == "Wirtschaftliche Zusammenarbeit und Entwicklung" ~ "entwicklung",
+  committee == "Europäische Angelegenheiten" ~ "europa",
+  committee == "Bildung, Forschung und Technikfolgenabschätzung" ~ "bildung_forschung",
+  committee == "Digitale Agenda" ~ "digitalisierung_technik",
+  committee == "Gesundheit" ~ "gesundheit",
+  committee == "Finanzen" ~ "haushalt_finanzen",
+  committee == "Haushalt" ~ "haushalt_finanzen",
+  committee == "Inneres" ~ "innere_sicherheit",
+  committee == "Kultur und Medien" ~ "kultur_medien_sport",
+  committee == "Sport" ~ "kultur_medien_sport",
+  committee == "Tourismus" ~ "kultur_medien_sport",
+  committee == "Ernährung und Landwirtschaft" ~ "landwirtschaft_ernaehrung",
+  committee == "Bau, Wohnen, Stadtentwicklung und Kommunen" ~ "soziales",
+  committee == "Familie, Senioren, Frauen und Jugend" ~ "soziales",
+  committee == "Umwelt, Naturschutz und nukleare Sicherheit" ~ "umwelt",
+  committee == "Verkehr und digitale Infrastruktur" ~ "verkehr",
+  committee == "Verteidigung" ~ "verteidigung",
+  committee == "Wirtschaft und Energie" ~ "wirtschaft"
+))
 
 
 # WP 20
@@ -73,6 +139,8 @@ committee_members_20 <- committee_members_20 %>%
   mutate(official_name = str_remove(fullname, remove_pattern)) %>% 
   distinct() # remove any duplicates in the data
 
+committee_members_20 <- committee_members_20 %>% 
+  mutate(committee = str_remove(committee, "_Mitglieder")) # remove "_Mitglieder" string from committee name
 
 ## Match Twitter IDs
 
@@ -89,6 +157,35 @@ committee_members_20 <- committee_members_20 %>% mutate(period = "WP_20",
                                                         end = NA)
 
 
+## add policy field
+
+committee_members_20 <- committee_members_20 %>% mutate(policy_field = case_when(
+  committee == "Arbeit und Soziales" ~ "arbeit",
+  committee == "Auswärtiger Ausschuss" ~ "aeusseres",
+  committee == "Menschenrechte und humanitäre Hilfe" ~ "entwicklung",
+  committee == "Wirtschaftliche Zusammenarbeit und Entwicklung" ~ "entwicklung",
+  committee == "Europäische Union" ~ "europa",
+  committee == "Bildung, Forschung und Technikfolgenabschätzung" ~ "bildung_forschung",
+  committee == "Digitales" ~ "digitalisierung_technik",
+  committee == "Gesundheit" ~ "gesundheit",
+  committee == "Finanzen" ~ "haushalt_finanzen",
+  committee == "Haushalt" ~ "haushalt_finanzen",
+  committee == "Inneres und Heimat" ~ "innere_sicherheit",
+  committee == "Kultur und Medien" ~ "kultur_medien_sport",
+  committee == "Sport" ~ "kultur_medien_sport",
+  committee == "Tourismus" ~ "kultur_medien_sport",
+  committee == "Ernährung und Landwirtschaft" ~ "landwirtschaft_ernaehrung",
+  committee == "Wohnen, Stadtentwicklung, Bauwesen und Kommunen" ~ "soziales",
+  committee == "Familie, Senioren, Frauen und Jugend" ~ "soziales",
+  committee == "Umwelt, Naturschutz, nukleare Sicherheit und Verbraucherschutz" ~ "umwelt",
+  committee == "Klimaschutz und Energie" ~ "umwelt",
+  committee == "Verteidigung" ~ "verteidigung",
+  committee == "Wirtschaft und Energie" ~ "wirtschaft",
+  committee == "Wirtschaft" ~ "wirtschaft",
+  committee == "Verkehr" ~ "verkehr"
+))
+
+
 ## bind
 
 committee_members <- committee_members_19 %>% bind_rows(committee_members_20)
@@ -96,4 +193,20 @@ committee_members <- committee_members_19 %>% bind_rows(committee_members_20)
 
 # save
 
-write_csv(committee_members, "committee_members_19-20_2023-04-05.csv")
+write_csv(committee_members, "committee_seeds_19-20_2023-04-06.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Seed_Terms/get_seed_terms.R
+++ Seed_Terms/get_seed_terms.R
@@ -1,26 +1,4 @@
-# Get data from HD Server #
+## Extract Seed Terms from Seed Accounts ##
+###########################################
 
 
-library(tidyverse)
-library(elastic)
-library(ssh)
-
-user = readline(prompt = "Enter Heidelberg Username: ") # enter credentials in console
-password = readline(prompt = "Enter Password: ") # user prompts for username and password, so the password is not visible in the script
-
-# host = "@wega.ifi.uni-heidelberg.de" # wega or adrastea to jump
-# 
-# jump <- ssh_connect(host = paste0(user, host),
-#             passwd = password)
-# 
-# tunnel <- ssh_tunnel(jump,  port = 9200, target = "erinome.ifi.uni-heidelberg.de:9200") # this command freezes the session?!
-
-conn <- connect(
-  path = "",
-  port = 9201,
-  user = user,
-  pwd = password,
-  host = "localhost"
-)
-
-conn$ping()
