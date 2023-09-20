44722f3e9a7a76e18605cbfb4f6fc9ef57b71c21
TimBMK <tim.b.m.koenig@web.de>
2023-09-21 00:03:22 +0200

    generalized tokenization for all non-tokenized data

--- news_classification/news_tokenization.R
+++ news_classification/news_tokenization.R
@@ -13,56 +13,61 @@
 
 cat("\nReading Data...\n")
 
-news_data <- vroom(file = "news_classification/data/data_news_2017-2018.csv.tar.gz") %>%
-  select(
-    # drop unnecessary columns
-    where(~ !all(is.na(.x))) &  # drop columns containing only NAs
-      !c(
-        "_index",
-        "_type",
-        "_score",
-        "_source.nlp_pipeline_applied",     # drop db internal information
-        "_source.lang",
-        "_source.publication_date",
-        "_source.crawled",
-        "_source.extracted"
-      ) # drop redundant information (estimated date is the only important date)
-  ) %>%
-  rename_with( ~ str_remove(.x, pattern = "_source.")) %>%  # drop "_source." from variable names
-  rename("id" = "_id") # drop "_" from id
+data_files <- list.files("news_classification/data", pattern = "data_news", full.names = T)
+
+# drop data already tokenized
+tokenized <- list.files("news_classification", pattern = "tokens_news", full.names = T)
+
+data_files <- data_files[!str_extract(data_files, "[\\d-]+") %in% str_extract(tokenized, "[\\d-]+")]
+
+news_data <- data_files %>% 
+  map(\(file)
+      {
+        data <- vroom(file = file) %>%
+          select(
+            # drop unnecessary columns
+            where(~ !all(is.na(.x))) &  # drop columns containing only NAs
+              !c(
+                "_index",
+                "_type",
+                "_score",
+                "_source.nlp_pipeline_applied",     # drop db internal information
+                "_source.lang",
+                "_source.publication_date",
+                "_source.crawled",
+                "_source.extracted"
+              ) # drop redundant information (estimated date is the only important date)
+          ) %>%
+          rename_with( ~ str_remove(.x, pattern = "_source.")) %>%  # drop "_source." from variable names
+          rename("id" = "_id") # drop "_" from id
+        return(data)
+  })
+
+names(news_data) <- str_extract(data_files, "[\\d-]+") # name the dataframes in the list
+
 
 # Tokenization, Lemmatization, Noun-Word Filtering
 
-cat("\nPreparing Data...\n")
 
-news_corpus <- corpus(news_data, docid_field = "id", text_field = "body",
-                      meta = list("host", "estimated_date", "url"), # preserve meta data (drop summary column)
-                      unique_docnames = T)
-
-
-cat("\nRunning Spacy....\n")
+cat("\nRunning Mapped Spacy....\n")
 
 spacy_initialize(model = "de_core_news_lg") # start python spacy
 
-news_tokens_201718 <- spacy_parse(news_corpus, 
-                                  pos = T, tag = T, lemma = T, 
-                                  entity = T, nounphrase = T,
-                                  additional_attributes = c("like_url", "like_num", "is_currency"))
+iwalk(\(data, name)
+    {corpus(data, docid_field = "_id", text_field = "_source.text", 
+                      unique_docnames = T) %>% 
+        spacy_parse(corpus,
+                           pos = T,
+                           tag = T,
+                           lemma = T,
+                           entity = T
+      ) %>% 
+        mutate(across(.cols = where(is.character),  ~ utf8::as_utf8(.x))) %>% 
+        vroom_write(file = paste0("news_classification/tokens_news_", name,".csv.tar.gz"), delim = ",")
+      
+})
 
 
 cat("\nTokenization complete. Finalizing spacy...\n")
 
 spacy_finalize() # end spacy
-
-
-
-# save
-
-cat("\nSaving...\n")
-
-news_tokens_201718 %>% 
-  mutate(across(.cols = where(is.character),  ~ utf8::as_utf8(.x))) %>% 
-  vroom_write(file = "news_classification/tokens_news_2017-2018.csv.tar.gz", delim = ",")
-
-
-cat("\nDone.")
